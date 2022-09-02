b238af144e6dcb0c01423afffaf99cc092fcf328
AmpereNinja <hmmahmudurrashidshaiyan@yahoo.com>
2022-09-02 18:57:12 +0600

    1 file modified

--- component/button/button.js
+++ component/button/button.js
@@ -1,14 +1,17 @@
-class Button extends HTMLElement {
-  constructor() {
-    super();
-    const state = this.getAttribute('text');
-    const wrapper = document.createElement('div');
-    const button = document.createElement('button')
-    button.innerText = state;
-
-    wrapper.appendChild(button);
-    this.append(wrapper);
-  }
-}
-
-customElements.define('nokusja-button', Button);
+(function () {
+  customElements.define(
+    'nokusja-button',
+    class extends HTMLElement {
+      constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        const wrapper = document.createElement('div');
+        const button = document.createElement('button');
+        button.innerHTML = this.innerHTML;
+        button.classList += 'notsuka-button';
+        wrapper.append(button);
+        this.shadowRoot.append(wrapper);
+      }
+    }
+  );
+});
